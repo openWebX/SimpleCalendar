38af3a8981764f5b012841085e8126affbee02ad
Jens Reinemuth <jens@reinemuth.info>
2017-11-06 02:14:13 +0100

    Added css to code!

--- lib/Calendar.php
+++ lib/Calendar.php
@@ -5,8 +5,8 @@ namespace SimpleCalendar {
     /**
      * Simple Calendar
      *
-     * @author Jesse G. Donat <donatj@gmail.com>
-     * @link http://donatstudios.com
+     * @author  Jesse G. Donat <donatj@gmail.com>
+     * @link    http://donatstudios.com
      * @license http://opensource.org/licenses/mit-license.php
      *
      */
@@ -17,31 +17,38 @@ namespace SimpleCalendar {
          *
          * @var array|false
          */
-        public $wday_names = false;
+        public $wday_names = FALSE;
 
+        /**
+         * @var
+         */
         private $now;
-        private $dailyHtml = array();
+        private $dailyHtml = [];
         private $offset = 0;
 
         /**
          * Constructor - Calls the setDate function
          *
          * @see setDate
+         *
          * @param null|string $date_string
          */
-        public function __construct( $date_string = null ) {
+        public function __construct(?string $date_string = NULL) {
             $this->setDate($date_string);
         }
 
         /**
          * Sets the date for the calendar
          *
-         * @param null|string $date_string Date string parsed by strtotime for the calendar date. If null set to current timestamp.
+         * @param null|string $date_string Date string parsed by strtotime for
+         *                                 the calendar date. If null set to
+         *                                 current timestamp.
          */
-        public function setDate( $date_string = null ) {
-            if( $date_string ) {
+        public function setDate(?string $date_string = NULL): void {
+            if ($date_string) {
                 $this->now = getdate(strtotime($date_string));
-            } else {
+            }
+            else {
                 $this->now = getdate();
             }
         }
@@ -49,24 +56,27 @@ namespace SimpleCalendar {
         /**
          * Add a daily event to the calendar
          *
-         * @param string      $html The raw HTML to place on the calendar for this event
-         * @param string      $start_date_string Date string for when the event starts
-         * @param null|string $end_date_string Date string for when the event ends. Defaults to start date
+         * @param string      $html              The raw HTML to place on the
+         *                                       calendar for this event
+         * @param string      $start_date_string Date string for when the event
+         *                                       starts
+         * @param null|string $end_date_string   Date string for when the event
+         *                                       ends. Defaults to start date
          */
-        public function addDailyHtml( $html, $start_date_string, $end_date_string = null ) {
+        public function addDailyHtml(string $html, string $start_date_string, ?string $end_date_string = NULL): void {
             static $htmlCount = 0;
             $start_date = strtotime($start_date_string);
             $end_date = $start_date;
-            if( $end_date_string ) {
+            if ($end_date_string) {
                 $end_date = strtotime($end_date_string);
             }
             $working_date = $start_date;
             do {
-                $tDate        = getdate($working_date);
+                $tDate = getdate($working_date);
                 $working_date += 86400;
 
                 $this->dailyHtml[$tDate['year']][$tDate['mon']][$tDate['mday']][$htmlCount] = $html;
-            } while( $working_date < $end_date + 1 );
+            } while ($working_date < $end_date + 1);
 
             $htmlCount++;
         }
@@ -74,17 +84,21 @@ namespace SimpleCalendar {
         /**
          * Clear all daily events for the calendar
          */
-        public function clearDailyHtml() { $this->dailyHtml = array(); }
+        public function clearDailyHtml() {
+            $this->dailyHtml = [];
+        }
 
         /**
          * Sets the first day of the week
          *
-         * @param int|string $offset Day to start on, ex: "Monday" or 0-6 where 0 is Sunday
+         * @param int|string $offset Day to start on, ex: "Monday" or 0-6 where
+         *                           0 is Sunday
          */
-        public function setStartOfWeek( $offset ) {
-            if( is_int($offset) ) {
+        public function setStartOfWeek($offset) :void {
+            if (is_int($offset)) {
                 $this->offset = $offset % 7;
-            } else {
+            }
+            else {
                 $this->offset = date('N', strtotime($offset)) % 7;
             }
         }
@@ -93,26 +107,29 @@ namespace SimpleCalendar {
          * Returns/Outputs the Calendar
          *
          * @param bool $echo Whether to echo resulting calendar
+         *
          * @return string HTML of the Calendar
          */
-        public function show( $echo = true ) {
-            if( $this->wday_names ) {
+        public function show( bool $echo = TRUE) :string {
+            if ($this->wday_names) {
                 $wdays = $this->wday_names;
-            } else {
+            }
+            else {
                 $today = (86400 * date('N'));
-                $wdays = array();
-                for( $i = 0; $i < 7; $i++ ) {
+                $wdays = [];
+                for ($i = 0; $i < 7; $i++) {
                     $wdays[] = strftime('%a', time() - $today + ($i * 86400));
                 }
             }
 
             $this->arrayRotate($wdays, $this->offset);
-            $wday    = date('N', mktime(0, 0, 1, $this->now['mon'], 1, $this->now['year'])) - $this->offset;
+            $wday = date('N', mktime(0, 0, 1, $this->now['mon'], 1, $this->now['year'])) - $this->offset;
             $no_days = cal_days_in_month(CAL_GREGORIAN, $this->now['mon'], $this->now['year']);
+            $out = "\n<style>\n" . file_get_contents(__DIR__ . '/css/SimpleCalendar.css') . "\n</style>\n";
 
-            $out = '<table cellpadding="0" cellspacing="0" class="SimpleCalendar"><thead><tr>';
+            $out .= '<table cellpadding="0" cellspacing="0" class="SimpleCalendar"><thead><tr>';
 
-            for( $i = 0; $i < 7; $i++ ) {
+            for ($i = 0; $i < 7; $i++) {
                 $out .= '<th>' . $wdays[$i] . '</th>';
             }
 
@@ -120,42 +137,44 @@ namespace SimpleCalendar {
 
             $wday = ($wday + 7) % 7;
 
-            if( $wday === 7 ) {
+            if ($wday === 7) {
                 $wday = 0;
-            } else {
+            }
+            else {
                 $out .= str_repeat('<td class="SCprefix">&nbsp;</td>', $wday);
             }
 
             $count = $wday + 1;
-            for( $i = 1; $i <= $no_days; $i++ ) {
+            for ($i = 1; $i <= $no_days; $i++) {
                 $out .= '<td' . ($i === $this->now['mday'] && $this->now['mon'] === date('n') && $this->now['year'] === date('Y') ? ' class="today"' : '') . '>';
 
                 $datetime = mktime(0, 0, 1, $this->now['mon'], $i, $this->now['year']);
 
                 $out .= '<time datetime="' . date('Y-m-d', $datetime) . '">' . $i . '</time>';
 
-                $dHtml_arr = false;
-                if( isset($this->dailyHtml[$this->now['year']][$this->now['mon']][$i]) ) {
+                $dHtml_arr = FALSE;
+
+                if (isset($this->dailyHtml[$this->now['year']][$this->now['mon']][$i])) {
                     $dHtml_arr = $this->dailyHtml[$this->now['year']][$this->now['mon']][$i];
                 }
 
-                if( is_array($dHtml_arr) ) {
-                    foreach( $dHtml_arr as $dHtml ) {
+                if (is_array($dHtml_arr)) {
+                    foreach ($dHtml_arr as $dHtml) {
                         $out .= '<div class="event">' . $dHtml . '</div>';
                     }
                 }
 
                 $out .= '</td>';
 
-                if( $count > 6 ) {
+                if ($count > 6) {
                     $out .= "</tr>\n" . ($i < $no_days ? '<tr>' : '');
                     $count = 0;
                 }
                 $count++;
             }
-            $out .= ( $count !== 1 ) ? str_repeat('<td class="SCsuffix">&nbsp;</td>', 8 - $count) . '</tr>' : '';
+            $out .= ($count !== 1) ? str_repeat('<td class="SCsuffix">&nbsp;</td>', 8 - $count) . '</tr>' : '';
             $out .= "\n</tbody></table>\n";
-            if( $echo ) {
+            if ($echo) {
                 echo $out;
             }
 
@@ -164,15 +183,15 @@ namespace SimpleCalendar {
 
         /**
          * @param array $data
-         * @param int $steps
+         * @param int   $steps
          */
-        private function arrayRotate( array &$data, $steps ) {
+        private function arrayRotate(array &$data, $steps) :void {
             $count = count($data);
-            if( $steps < 0 ) {
+            if ($steps < 0) {
                 $steps = $count + $steps;
             }
             $steps %= $count;
-            for( $i = 0; $i < $steps; $i++ ) {
+            for ($i = 0; $i < $steps; $i++) {
                 $data[] = array_shift($data);
             }
         }
